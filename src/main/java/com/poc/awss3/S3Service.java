2914ebe6eae37b3569dd321f62dfc635250f09d3
anuj-mehra <mehra.anuj1712@gmail.com>
2023-07-02 20:44:47 +0530

    code added to read property file

--- src/main/java/com/poc/awss3/S3Service.java
+++ src/main/java/com/poc/awss3/S3Service.java
@@ -1,9 +1,37 @@
 package com.poc.awss3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class S3Service {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        // Reading property file.
+        S3Service obj = new S3Service();
+        obj.loadPropertyFile();
+        System.out.println("username: "+ obj.prop.getProperty("s3.accessKey"));
+
         final S3ConnectionFactory connFactory = new S3ConnectionFactory();
 
     }
+
+    private Properties prop = null;
+
+    public void loadPropertyFile(){
+
+        InputStream is = null;
+        try {
+            prop = new Properties();
+            is = this.getClass().getResourceAsStream("/application.properties");
+            prop.load(is);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
